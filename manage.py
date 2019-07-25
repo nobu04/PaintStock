4fb531af741afda506dee3edb3c6fe2fe23b4155
nobu04 <ditch.mouse@gmail.com>
2019-07-25 23:52:45 +0900

    make it better

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE",
     #                       "<app_name>.config.settings.local_settings")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "<app_name>.config.settings.production_settings")
+                          "project.product_settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
