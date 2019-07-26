931dcd0e46ad78c239ba7cb45879f0425b4bdda0
nobu04 <ditch.mouse@gmail.com>
2019-07-26 15:06:26 +0900

    make it better

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "project.product_settings")
